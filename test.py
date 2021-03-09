55de728e708388da7baa7d66d8d9ca94a392e0ee
Gralalo <louis@cegullah.co.za>
2021-03-09 09:25:03 +0200

    + Added test function for string dictionary + Added test function for string decode

--- test.py
+++ test.py
@@ -1,14 +1,36 @@
+import zlib
+
 
 def sq_odd(nums):
-        odd_nums_squared = []
-        for num in nums:
-            is_odd = num % 2 != 0
-            if is_odd:
-                odd_nums_squared.append(num ** 2)
+    odd_nums_squared = []
+    for num in nums:
+        is_odd = num % 2 != 0
+        if is_odd:
+            odd_nums_squared.append(num**2)
+
+    return odd_nums_squared
 
 
-        return odd_nums_squared
-        
-my_list = [1,2,3,4,5,6,7,8,9]
+my_list = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
-print (sq_odd(my_list))
+print(sq_odd(my_list))
+
+
+def gen_dictionary(strings):
+    strings_dictionary = {}
+    for string in strings:
+        strings_dictionary[string] = zlib.compress(string.encode())
+    return strings_dictionary
+
+
+my_strings = ["louis", "la", "Grange", "works", "very", "hard", "LOoooooooooooooooooooooooooooooooooongs striiiiiiiiiiiiiiiinnnnnngggggggggggggg"]
+
+print(gen_dictionary(my_strings))
+
+def decode_string(string):
+    decoded_string = zlib.decompress(string)
+    return decoded_string.decode()
+
+my_string = b"x\x9c\xf3\xf1\xcf'\x08\xf2\xd2\x8b\x15\x8aK\x8a2\xd1@\x1e\x18\xa4\xa3\x00\x00,n \xe1"
+
+print(decode_string(my_string))
