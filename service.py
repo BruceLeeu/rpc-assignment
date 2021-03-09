358260a44004ad770910352205f24b34f60c0f3f
Gralalo <louis@cegullah.co.za>
2021-03-09 09:42:48 +0200

    * Implemented all initial functions for service

--- service.py
+++ service.py
@@ -1,6 +1,7 @@
 # RPC Service using nameko
 
 from nameko.rpc import rpc, RpcProxy
+import zlib
 
 class SquareOddNums:
     name = "square_odd_nums"
@@ -11,18 +12,24 @@ class SquareOddNums:
         for num in nums:
             is_odd = num % 2 != 0
             if is_odd:
-                odd_nums_squared.append(num ** 2)
-
+                odd_nums_squared.append(num**2)
 
         return odd_nums_squared
 
 class GenerateStringDictionary:
     name = "generate_string_dictionary"
 
-    # Call other remote service
-    # y = RpcProxy("service_y")
-
     @rpc
     def gen_dictionary(strings):
-        res = u"{}-x".format(value)
-        return self.y.append_identifier(res)
+        strings_dictionary = {}
+        for string in strings:
+            strings_dictionary[string] = zlib.compress(string.encode())
+        return strings_dictionary
+
+class DecodeString:
+    name = "decode_string"
+
+    @rpc
+    def decode_string(string):
+        decoded_string = zlib.decompress(string).decode()
+        return decoded_string
--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 import zlib
+import sys
 
 
 def sq_odd(nums):
@@ -7,7 +8,6 @@ def sq_odd(nums):
         is_odd = num % 2 != 0
         if is_odd:
             odd_nums_squared.append(num**2)
-
     return odd_nums_squared
 
 
@@ -23,13 +23,20 @@ def gen_dictionary(strings):
     return strings_dictionary
 
 
-my_strings = ["louis", "la", "Grange", "works", "very", "hard", "LOoooooooooooooooooooooooooooooooooongs striiiiiiiiiiiiiiiinnnnnngggggggggggggg"]
+my_strings = [
+    "louis", "la", "Grange", "works", "very", "hard",
+    "LOoooooooooooooooooooooooooooooooooongs striiiiiiiiiiiiiiiinnnnnngggggggggggggg"
+]
 
 print(gen_dictionary(my_strings))
 
+
 def decode_string(string):
-    decoded_string = zlib.decompress(string)
-    return decoded_string.decode()
+    # print("\nsize of compressed text", sys.getsizeof(string))
+    decoded_string = zlib.decompress(string).decode()
+    # print("\nsize of original text", sys.getsizeof(decoded_string))
+    return decoded_string
+
 
 my_string = b"x\x9c\xf3\xf1\xcf'\x08\xf2\xd2\x8b\x15\x8aK\x8a2\xd1@\x1e\x18\xa4\xa3\x00\x00,n \xe1"
 
